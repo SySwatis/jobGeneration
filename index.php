a3ae0931adfdbe00c315f6c2237711f2fdca4ad7
syswatis <syswatis@gmail.com>
2023-03-02 13:11:48 +0100

    First local commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<?php include_once('inc/head.php'); ?>
+
+<body>
+
+<div id="wrapper">
+
+    <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <?php include_once('inc/menu.php') ?>
+            
+        </div>
+    </nav>
+
+    <div id="page-wrapper" _class="gray-bg"   style="background-image:url('img/AdobeStock_526910736_Preview.webp');background-repeat:no-repeat;background-size:cover;">
+        
+        <?php include_once('inc/top-navbar.php') ?>     
+        <?php include_once('inc/dashboard.php') ?>
+        <?php include_once('inc/footer.php') ?>
+
+    </div>
+</div>
+
+<!-- Mainly scripts -->
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="js/inspinia.js"></script>
+<script src="js/plugins/pace/pace.min.js"></script>
+<script src="js/main.js"></script>
+
+
+</body>
+
+</html>
