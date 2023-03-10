610ae34d5df6ec5fe20f9ab93d39a492e9827f1d
syswatis <syswatis@gmail.com>
2023-03-10 14:40:09 +0100

    First commit

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@ function get_menu() {
     $array =array(
         array('Accueil','house','index'),
         array('Agenda', 'calendar','agenda'),
-        array('Bénéficiiares','users','beneficiaires'),
+        array('Bénéficiaires','users','beneficiaires'),
         array('Gestion','gear','gestion'),
         array('Outils','list-check','outils')
     );
