e2828f667d6c020cc7714b738afb2811420d92cb
syswatis <syswatis@gmail.com>
2023-03-20 16:52:12 +0100

    Starter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 (function ($) {
 
+    // login
+
+    function scrollToElement(targetEl) {
+        $targetEl = $(targetEl);
+        $('html, body').stop().animate({
+            'scrollTop': $targetEl.offset().top
+        }, 100, 'swing', function () {
+            window.location.hash = targetEl;
+        });
+    }
+
+    $('#loginForm input').on('click',function(e){
+        e.preventDefault();
+        scrollToElement('#loginForm');
+    })
+
 })(jQuery);
--- login.php
+++ login.php
@@ -18,7 +18,7 @@
                 <p class="text-muted text-left small">Utilisez vos identifiants habituels
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <form class="m-t" role="form" action="index.php">
+            <form id="loginForm" class="m-t" role="form" action="index.php">
                 <div class="form-group mb-3">
                     <input type="text" class="form-control" placeholder="Login" required>
                 </div>
@@ -29,9 +29,7 @@
               
             </form>
             <a href="#a-propos" class="text-muted"><small>A propos</small></a>
-
-            <p class="text-white text-center mt-2 mb-0"><small>Un problème de connection ?</small></p>
-            <a href="#contact" class="text-muted"><small>Contact</small></a>
+             | <a href="#contact" class="text-muted"><small>Un problème de connection ?</small></a>
         </div>
     </div>
 </div>
